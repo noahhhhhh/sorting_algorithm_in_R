956197f5e9c4c05beaecb47ddd3f9fdfd79db856
noahhhhhh <noah.xm.shaw@gmail.com>
2015-09-07 23:40:49 +1000

    merge

--- MergeSort.R
+++ MergeSort.R
@@ -2,8 +2,29 @@
 ## merge sort #################################################################################
 ###############################################################################################
 
-items <- sample(1:10, 5, replace = T)
+items <- c(18, 16, 8, 7, 6, 3, 11, 9, 15, 1, 1, 1)
 items
+
+MergeCompare <- function(left, right){
+    lenLeft <- length(len)
+    lenRight <- length(right)
+    
+    newItems <- numeric(lenLeft + lenRight)
+    lenNewItems <- lenLeft + lenRight
+    i_left <- 1
+    i_right <- 1
+    i <- 1
+    for (i in 1:lenNewItems){
+        if ((left[i_left] < right[i_right] && i_left <= lenLeft) || i_right > lenRight){
+            newItems[i] <- left[i_left]
+            i_left <- i_left + 1
+        } else {
+            newItems[i] <- right[i_right]
+            i_right <- i_right + 1
+        }
+    }
+    newItems
+}
 MergeSort <- function(items){
     len <- length(items)
     if (len > 1){
@@ -11,29 +32,12 @@ MergeSort <- function(items){
         left <- items[1:sep]
         right <- items[(sep + 1):len]
         
-        lenLeft <- length(len)
-        lenRight <- length(right)
+        mLeft <- MergeSort(left)
+        mRight <- MergeSort(right)
         
-        MergeSort(left)
-        MergeSort(right)
-        
-        newItems <- numeric(lenLeft + lenRight)
-        lenNewItems <- lenLeft + lenRight
-        i_left <- 1
-        i_right <- 1
-        i <- 1
-        for (i in 1:lenNewItems){
-            if ((left[i_left] < right[i_right] & i_left <= lenLeft) | i_right > lenRight){
-                newItems[i] <- left[i_left]
-                i_left <- i_left + 1
-            } else if ((left[i_left] >= right[i_right] & i_right <= lenRight) | i_left > lenLeft){
-                newItems[i] <- right[i_right]
-                i_right <- i_right + 1
-            }
-        }
-        return(newItems)
+        MergeCompare(mLeft, mRight)
     } else {
-        return(items)
+        items
     }
 }
 
