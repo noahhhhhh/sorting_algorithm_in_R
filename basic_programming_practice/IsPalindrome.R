4e09daedb7eead75eaecee64bc6d53c79e9c2811
noahhhhhh <noah.xm.shaw@gmail.com>
2015-09-12 11:25:10 +1000

    palindrome

--- basic_programming_practice/IsPalindrome.R
+++ basic_programming_practice/IsPalindrome.R
@@ -1,4 +1,18 @@
 ###############################################################################################
 ## is palindrome ##############################################################################
 ###############################################################################################
-## palindrome:
+## palindrome: abcs22scba, acasaca, etc.
+IsPalindrome <- function(string){
+    len <- nchar(string)
+    count <- 0
+    isOdd <- 
+    for (i in 1:floor(len/2)){
+        if(substr(string, i, i) != substr(string, len - i + 1, len - i + 1)){
+            count <- count + 1
+            return(F)
+        }
+    }
+    if (count == 0){
+        return(T)
+    }
+}
